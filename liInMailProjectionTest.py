c9d837bf3defcbfb476ff0d39000f7c5674521cd
Joe Klonowski <joe.klonowski@4cinsights.com>
2018-07-24 14:54:24 -0500

    Add test for InMail download projection.

---
+++ liInMailProjectionTest.py
@@ -0,0 +1,11 @@
+from voxsup.api.client.linkedin import LinkedInClient as Client
+account_id = 500735271 # VoxSup Inc
+client = Client(context={'account_id': account_id})
+enable_requests_logging()
+
+params = {
+    'ids': [930516],
+    'projection': '(results(*(*,subContent(*(*,adUnitV2~:playableStreams)))))',
+}
+response = client.get('adInMailContentsV2', params=params)
+response.json()
