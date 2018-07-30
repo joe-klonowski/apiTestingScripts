ea244e02317be4ca4507545b7bc1e9e5de1fbf72
Joe Klonowski <joe.klonowski@4cinsights.com>
2018-07-30 17:11:50 -0500

    Remove JSON stuff from sender permissions put test so that it works.

--- liSenderPermissionsPutTest.py
+++ liSenderPermissionsPutTest.py
@@ -1,16 +1,10 @@
 from voxsup.api.client.linkedin import LinkedInClient as Client
-ACCOUNT_ID = 500735271 # VoxSup Inc
+ACCOUNT_ID = 502977261 # 4C Insights Test Account
 ACCOUNT_URN = "urn:li:sponsoredAccount:" + str(ACCOUNT_ID)
-COMPANY_ID = 3026918 # VoxSup Inc
-MEMBER_ID = "YvvwSuc8fR" # Salo
+MEMBER_ID = "-bZaJqJQEO" # Noopur
 MEMBER_URN = "urn:li:person:" + MEMBER_ID
 client = Client(context={'account_id': ACCOUNT_ID})
 enable_requests_logging()
-
-params = {
-    "account": ACCOUNT_URN,
-    "member": MEMBER_URN,
-}
 
 body = {
     "account": ACCOUNT_URN,
@@ -18,9 +12,8 @@ body = {
     "state": "REQUESTED",
 }
 
-# response = client.put('adInMailMemberSenderPermissions', params=params, json=body)
 urlString = 'adInMailMemberSenderPermissions/account=' + ACCOUNT_URN + '&member=' + MEMBER_URN
 response = client.put(urlString, json=body)
-content = response.json()
-import json
-json.dumps(content)
+# content = response.json()
+# import json
+# json.dumps(content)
