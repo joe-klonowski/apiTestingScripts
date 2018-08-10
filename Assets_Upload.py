a0159e2e46f6b4157be597a15be3afaa0c214bfd
Joe Klonowski <joe.klonowski@4cinsights.com>
2018-08-10 14:36:58 -0500

    My changes to Pooja's script.

--- Assets_Upload.py
+++ Assets_Upload.py
@@ -1,20 +1,25 @@
 import json
 import requests
 
+account_id = 500735271 # VoxSup Inc
+COMPANY_ID = 3026918 # VoxSup Inc
+INMAIL_ID = 1225113 # Testing preview
+AUTH_TOKEN = 'AQWXKBmSvxt40kGcvg92Jn-1QUIyXczXmrzMayteVOeQ3a5eFfqJH746_TInOgmTMrhbwSDIKDZX0-tT4CUVFCx3okfXSoICslJaI3srNy9Xit2nH7DBuXz_yB09Lmj6QSwjv0sXea87Wtq7dcztfFgdSpcAYQIXp9rmxQx9dNWa2RhHf4wqeFxAmoUXZF5UfGPlqrcynp0d3z-Zn1eGlqC7TxElBy3kns0J0L92ztbH0-8KVxm0xloCbGmaawG9wUXBEfLn0Z5N0GBzXmICP-C3XYAgnIMLAPOMtIbKksPd2aGLEFzwfVjtXf6EHdxj5QlXNh3cDW7GjH_xDu49eF7G_Y_trw'
+FILE_PATH = '/home/joeklonowski/voxsupFrontend2/image.png'
 
 register_upload = {
 	"url": "https://api.linkedin.com/v2/assets?action=registerUpload",
-	"headers": {'Content-Type': 'application/json',"Authorization": "Bearer <TOKEN>"},
+	"headers": {'Content-Type': 'application/json',"Authorization": "Bearer " + AUTH_TOKEN},
 	"request_body": {
-  "registerUploadRequest": {
-    "owner": "urn:li:company:<company_id>",
-    "serviceRelationships":[{"relationshipType":"OWNER","identifier":"urn:li:userGeneratedContent"}],
-    "recipes": [
-      "urn:li:digitalmediaRecipe:feedshare-video"
-    ]
-  }
+      "registerUploadRequest": {
+        "owner": "urn:li:company:" + str(COMPANY_ID),
+        "serviceRelationships":[{"relationshipType":"OWNER","identifier":"urn:li:userGeneratedContent"}],
+        "recipes": [
+            "urn:li:digitalmediaRecipe:spinmail-banner-image"
+        ]
+      }
+    }
 }
-	}
 
 
 
@@ -31,20 +36,19 @@ print "The video asset id is "+ data["value"]["asset"]
 
 print "\n \n \n-------- Step 2 upload url -----"
 
-upload_file = "/Users/pnihalan/Downloads/SampleVideo_640x360_2mb.mp4"
 upload_asset = {
 	"headers": data["value"]["uploadMechanism"]["com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"]["headers"],
 	"url":data["value"]["uploadMechanism"]["com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"]["uploadUrl"]}
-files = {'upload_file': open(upload_file, 'rb')}
+files = {'upload_file': open(FILE_PATH, 'rb')}
 
 
 import commands
 #Creating curl call
-curl_call = "curl -v " 
+curl_call = "curl -v "
 for k,v in data["value"]["uploadMechanism"]["com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"]["headers"].items():
 	curl_call = curl_call+" -H '" + k+":"+v +"'"
 
-curl_call =curl_call+ " --upload-file "+upload_file+ " '"+ data["value"]["uploadMechanism"]["com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"]["uploadUrl"]+"' "
+curl_call =curl_call+ " --upload-file "+FILE_PATH+ " '"+ data["value"]["uploadMechanism"]["com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"]["uploadUrl"]+"' "
 status, output = commands.getstatusoutput(curl_call)
 #print output
 print "Video uploaded"
