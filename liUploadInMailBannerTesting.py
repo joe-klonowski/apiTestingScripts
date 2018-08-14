6e2bdae9356a5f913acc8ca53ae2026486bd873d
Joe Klonowski <joe.klonowski@4cinsights.com>
2018-08-14 14:21:49 -0500

    Tried to get banner thing working one last time, didn't go.

--- liUploadInMailBannerTesting.py
+++ liUploadInMailBannerTesting.py
@@ -1,12 +1,9 @@
 from voxsup.api.client.linkedin import LinkedInClient as Client
-from requests import Request, Session
-
 account_id = 500735271 # VoxSup Inc
 COMPANY_ID = 3026918 # VoxSup Inc
 INMAIL_ID = 1225113 # Testing preview
-AUTH_TOKEN = 'AQUpNzFf2yuWxcy82SyzphIC2LCSPzLwMMnr08PrU1Gkcp5k7v0hQj_TF04z9AdIVsAd4dHV7GUaRnBdkMla747STTEtbMDDHixJYjJrFHxsNpv3EmJ8XV3OnDPtJWwO3N7k7SR-dN0UnEpVdwRjrojZIj1W6Sz_W6Bz1Ooyl764PrIakPPV9OklNBXQ0GrsttIkjU8smvG6_79VgtQeCv53CY7lhY1E1QnXjjt2d_gTpMkkWAF5QZWnO8ccSHjNgVYAf8_512swlaPN8dWBw5gOXWA5eNUHAdOSZDixYqLth_59oZoNAwXsWTmA8fMAOKhxFcpO_ltPqOBlHXtIfcSgKvu4lQ'
 client = Client(context={'account_id': account_id})
-enable_requests_logging()
+# enable_requests_logging()
 
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
@@ -47,45 +44,33 @@ print('headers: ' + json.dumps(headers))
 print('uploadUrl: ' + json.dumps(uploadUrl))
 print('assetId: ' + assetId)
 
-
-files = {'file': ("image.png", open('/home/joeklonowski/voxsupFrontend2/image.png', 'rb'), 'image/png')}
-uploadRequest = Request('PUT', uploadUrl, files=files)
-preppedRequest = uploadRequest.prepare()
-del preppedRequest.headers['Content-Type']
-# del preppedRequest.headers['Content-Length']
-preppedRequest.headers['Authorization'] = 'Bearer ' + AUTH_TOKEN
-uploadResponse = client.send(preppedRequest)
-
-# uploadHeaders = {
-#   'Content-Type': None,
-#   # 'Content-Length': '189435'
-# }
-# uploadResponse = client.put(uploadUrl, files=files, headers=uploadHeaders)
-
-getHeaders = {
-  'Cache-Control': 'no-cache'
+uploadHeaders = {
+  'Content-Type': 'image/png'
 }
-getResponse = client.get('assets/' + assetId, headers=getHeaders)
-json.dumps(getResponse.content)
+files = {'file': ("image.png", open('/home/joeklonowski/voxsupFrontend2/image.png', 'rb'), 'image/png')}
+uploadResponse = client.put(uploadUrl, files=files, headers=uploadHeaders)
 
-# updateBody = {
-#   "patch": {
-#     "$set": {
-#       "subContent": {
-#         "com.linkedin.ads.AdInMailStandardSubContent": {
-#           "actionText": "HMC",
-#           "action": "https://www.hmc.edu/",
-#           "adUnitV2": asset
-#         }
-#       }
-#     }
-#   }
-# }
-# updateResponse = client.post('adInMailContentsV2/' + str(INMAIL_ID), json=updateBody)
-# updateResponse.json()
+getResponse = client.get('assets/' + assetId)
+getResponse.json()
 
-# getInMailParams = {
-#   'projection': '(id,subContent(com.linkedin.ads.AdInMailStandardSubContent(adUnitV2~:playableStreams)))'
-# }
-# getInMailResponse = client.get('adInMailContentsV2/' + str(INMAIL_ID), params=getInMailParams)
-# getInMailResponse.json()
+updateBody = {
+  "patch": {
+    "$set": {
+      "subContent": {
+        "com.linkedin.ads.AdInMailStandardSubContent": {
+          "actionText": "HMC",
+          "action": "https://www.hmc.edu/",
+          "adUnitV2": asset
+        }
+      }
+    }
+  }
+}
+updateResponse = client.post('adInMailContentsV2/' + str(INMAIL_ID), json=updateBody)
+updateResponse.json()
+
+getInMailParams = {
+  'projection': '(id,subContent(com.linkedin.ads.AdInMailStandardSubContent(adUnitV2~:playableStreams)))'
+}
+getInMailResponse = client.get('adInMailContentsV2/' + str(INMAIL_ID), params=getInMailParams)
+getInMailResponse.json()
